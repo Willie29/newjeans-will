12abf7c97aa6e89f00b72b82feea7041f046d24f
Willyam <willdyan29@gmail.com>
2023-10-10 22:26:18 +0700

    fixing the shrinking issue and changing twitter logo

--- js/member.js
+++ js/member.js
@@ -24,10 +24,12 @@ const members = [
 const memberList = document.getElementById('memberList');
 members.forEach((member) => {
     const listItem = document.createElement('li');
-    listItem.className = 'flex flex-col gap-2 items-center w-[250px] transition-all ease-in-out duration-500 hover:scale-110 hover:drop-shadow-[20px_25px_50px_rgba(0,0,0,1)] hover:text-white';
+    listItem.className = 'w-[250px] transition-all ease-in-out duration-500 hover:scale-110 hover:drop-shadow-[20px_25px_50px_rgba(0,0,0,1)] hover:text-white';
     listItem.innerHTML = `
-        <img src="https://storage.googleapis.com/nj-img/${member.imageURL}" alt="${member.name}" class="rounded-[20px]" title="${member.name}">
-        <h3 class="text-[20px]">${member.name}</h3>
+        <a href="#member" class="flex flex-col gap-2 items-center">
+            <img src="https://storage.googleapis.com/nj-img/${member.imageURL}" alt="${member.name}" class="rounded-[20px]" title="${member.name}">
+            <h3 class="text-[20px]">${member.name}</h3>
+        </a>
     `;
     memberList.appendChild(listItem);
 });
